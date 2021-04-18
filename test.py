44393b033a172955643b6830be4ac4f5161fe081
sbchild <a15315862177@163.com>
2021-04-18 09:06:40 +0800

    随机目录名

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import getLinks
 import getVideoLink
 import decryptLink
+import time
 
 # 仅测试
 ua = ""  # "-user_agent " + '"User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36"'
@@ -13,21 +14,29 @@ def main():
     link_urls = lg.run()
     f1 = open("dld.sh", "w")
     f2 = open("dld_windows.bat", "w")
-    f1.write("mkdir videos\n")
-    f2.write("mkdir videos\n")
+    videos_dir = "videos_" + str(int(time.time()))
+    print(f"生成的脚本将自动创建 {videos_dir} 目录")
+    f1.write(f"mkdir {videos_dir}\n")
+    f2.write(f"mkdir {videos_dir}\n")
     count = 0
     link_urls.reverse()
+    f1.write(f"# all: {len(link_urls)}\n\n")
+    f2.write(f":: all: {len(link_urls)}\n\n")
     for link_url in link_urls:
-        print(link_url)
+        print(f"视频{count}:", link_url)
         this_link = getVideoLink.getLink(link_url[0])
         # print(this_link)
         this_url = dl.decrypt(this_link)
-        # print(this_url)
+        print(f"视频{count}的链接:", this_url)
         f1.write(f"# {count}: {link_url[1]}\n")
         f2.write(f":: {count}: {link_url[1]}\n")
+        f1.write(f"# img: {link_url[2]}\n")
+        f2.write(f":: img: {link_url[2]}\n")
         url_fix = this_url.replace('&', '\\&')
-        f1.write(f"ffmpeg {ua} {headers} -i {url_fix} -c copy videos/{count}.mp4\n")
-        f2.write(f"ffmpeg {ua} {headers} -i {this_url} -c copy videos/{count}.mp4\n")
+        f1.write(f"ffmpeg {ua} {headers} -i {url_fix} -c copy {videos_dir}/{count}.mp4\n")
+        f2.write(f"ffmpeg {ua} {headers} -i {this_url} -c copy {videos_dir}/{count}.mp4\n")
+        f1.write("\n")
+        f2.write("\n")
         # ffmpeg -i _url -c copy _file
         count += 1
     f1.close()
