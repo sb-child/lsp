1458d305c3494f0ee5cdff6fddfc1307e0a08beb
sbchild <a15315862177@163.com>
2021-05-16 18:39:37 +0800

    合并视频时, 解密ts文件

--- downloader.py
+++ downloader.py
@@ -1,12 +1,12 @@
 # 下载模块
 import os
-import shutil
 import subprocess
 import uuid
 import retrying
 import requests
 from typing import Union
 from tqdm import tqdm
+from Crypto.Cipher import AES
 
 
 def urlGet(url: str):
@@ -47,6 +47,10 @@ def downloadM3u8(link: dict[str, Union[str, list[str], tuple[str, str, str], flo
     video_len: float = link["len"]
     videos_list_len = len(videos_list)
     uid = uuid.uuid4().__str__()
+
+    if video_encrypt == "":
+        return 3
+
     for i in tqdm(range(videos_list_len), desc="下载视频"):
         dld_url = videos_list[i]
         fn = os.path.join(out_dir, f"t_{uid}_{i}.ts")
@@ -54,38 +58,20 @@ def downloadM3u8(link: dict[str, Union[str, list[str], tuple[str, str, str], flo
             downloadVideoPart(dld_url=dld_url, filename=fn)
         except Exception:
             raise ConnectionError("多次下载失败, 放弃.")
+
     fn = os.path.join(out_dir, f"t2_{uid}.ts")
     for i in tqdm(range(videos_list_len), desc="合并视频"):
         fn2 = os.path.join(out_dir, f"t_{uid}_{i}.ts")
         with open(fn2, "rb") as f:
             data = f.read()
+        if video_encrypt != "":
+            keyStr = urlGetToStr(video_encrypt)
+            keyBin = keyStr.encode()
+            aesDec = AES.new(keyBin, AES.MODE_CBC, keyBin)
+            data = aesDec.decrypt(data)
         with open(fn, "ab+") as f:
             f.write(data)
         os.remove(fn2)
-    if video_encrypt != "":
-        print("解密视频, 这通常需要很长时间...")
-        fn_key = os.path.join(out_dir, f"t_key_{uid}.key")
-        fn_dec_m3u8 = os.path.join(out_dir, f"t_dec_{uid}.m3u8")
-        fn_dec_out = os.path.join(out_dir, f"t_dec_out_{uid}.ts")
-        urlGetToBinFile(video_encrypt, fn_key)
-        with open(fn_dec_m3u8, "w") as f:
-            f.write(f"""
-#EXTM3U
-#EXT-X-VERSION:3
-#EXT-X-MEDIA-SEQUENCE:0
-#EXT-X-KEY:METHOD=AES-128,URI="{os.path.split(fn_key)[-1]}"
-#EXTINF:{'{:.6f}'.format(video_len)},
-{os.path.split(fn)[-1]}
-            """.strip())
-        if subprocess.run(f"ffmpeg -allowed_extensions ALL "
-                          f"-v 0 -y -i {fn_dec_m3u8} -c copy "
-                          f"{fn_dec_out}", shell=True).returncode != 0:
-            print("解密时出错.")
-            return 2
-        os.remove(fn_key)
-        os.remove(fn)
-        shutil.move(fn_dec_out, fn)
-        print("解密完成.")
 
     print("转换为mp4格式...")
     fn3 = os.path.join(out_dir, f"{out_file}.mp4")
@@ -93,12 +79,14 @@ def downloadM3u8(link: dict[str, Union[str, list[str], tuple[str, str, str], flo
         print("格式转换时出错.")
         return 1
     os.remove(fn)
+
     print("下载封面...")
     fn_img = os.path.join(out_dir, f"{out_file}.jpg")
     urlGetToBinFile(link_url[2], fn_img)
+
     print("写出描述文件...")
     fn_desc = os.path.join(out_dir, f"{out_file}.txt")
     with open(fn_desc, "w") as f:
         f.write("\n".join(link_url))
+
     print("完成.")
-    pass
